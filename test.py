cad90566db3c0364656171b9627579eb24bd7533
meee-low <my_milo_m@hotmail.com>
2023-08-19 11:45:35 -0300

    Better example

--- test.py
+++ test.py
@@ -4,21 +4,22 @@ import flags
 
 def main() -> None:
     # Initiate a flag handler
-    fh = flags.FlagHandler("""This program tests the flags module made by \
-Matheus Ferreira Drumond (github.com/meee-low).""")
+    fh = flags.FlagHandler("This program tests the flags module made by github.com/meee-low.")
 
     # Add flags
     name        = fh.str_flag("-n", "The name to be greeted!", optional=False, aliases=["--name"])
     count       = fh.int_flag("-c", "How many times to greet!", "1", aliases=["--count"])
-    insult_flag = fh.bool_flag("--insult", "Insults the person with the name.")
+    praise_flag = fh.bool_flag("--praise", "Praises the person with the name.")
 
     # Parse the command line arguments
     fh.parse(sys.argv)
 
     # Free to use the data stored in each flag now!
+    print(name.data, count.data, praise_flag.data)
     for _ in range(count.data):
-        insult = ", you buffoon" if insult_flag.data else ""
-        print(f"Hello, {name.data}{insult}!")
+        print(f"Hello, {name.data}!")
+    if praise_flag.data:
+        print(f"You're looking good today, {name.data}!")
 
 
 if __name__ == "__main__":
